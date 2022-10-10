5b6bf06858ce34c8df75d47ba417b1e037960684
Crow <ajimenez@invercratos.io>
2022-10-10 15:27:48 -0500

    Se me fue una linea de más

--- cactus/cactus/utils/unblock.py
+++ cactus/cactus/utils/unblock.py
@@ -11,7 +11,6 @@ def unblock_account(_user_, time_):
         tiempo = (compare - time_) > datetime.timedelta(minutes=5)
         if tiempo:
             _user_.Uprofile.login_attempts = 0
-            _user_.Uprofile.blocked_times = 0
             _user_.Uprofile.blocked_reason = "K"
             _user_.Uprofile.status = "O"
             _user_.Uprofile.save()
