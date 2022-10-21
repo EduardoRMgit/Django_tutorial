4aa33030601ab9b4fb239504741dea76b1e649af
kanbalam <albertkanbalam@gmail.com>
2022-10-21 10:00:07 -0500

    Respuestas servicios Dapp

--- cactus/dapp/views/codigoView.py
+++ cactus/dapp/views/codigoView.py
@@ -1,27 +1,29 @@
+import logging
+
 from dapp.models import CodigoCobro
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-import logging
+
+
+db_logger = logging.getLogger("db")
 
 
 @api_view(['POST'])
 def CodigoCobroView(request):
-    db_logger = logging.getLogger("db")
-
     if request.method == 'POST':
-        user = request.data["user"]
-        code = request.data["code"]
-        security = request.data["security"]
+        # user = request.data["user"]
+        # code = request.data["code"]
+        # security = request.data["security"]
         cuerpo = {}
-        cuerpo['rc'] = '0'
-        cuerpo['msg'] = 'OK'
-        cuerpo['user'] = user
-        cuerpo['code'] = code
-        cuerpo['security'] = security
+        cuerpo['rc'] = 0
+        cuerpo['msg'] = "Operación exitosa"
+        # cuerpo['user'] = user
+        # cuerpo['code'] = code
+        # cuerpo['security'] = security
         procesado = cuerpo
-        CodigoCobro.objects.create(user=user,
-                                   code=code,
-                                   security=security,
+        CodigoCobro.objects.create(user='user PRUEBA',
+                                   code='code PRUEBA',
+                                   security='security PRUEBA',
                                    json=request.data)
-        db_logger.info(request.data)
+        db_logger.info("[DAPP codigoCobro]: {}".format(request.data))
         return Response(procesado)
