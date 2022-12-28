b0e22e153240109784deac944e5a1e7ba761bd2e
DannielXdx <dannielxdx@outlook.com>
2022-12-28 11:58:40 -0600

    avance de comprobantes

---
+++ cactus/banca/utils/comprobantesPng.py
@@ -0,0 +1,31 @@
+import cv2
+
+
+class Comprobante():
+    def __init__(self) -> None:
+        print(self)
+        pass
+
+    def cobro(cobro):
+        return ("hola")
+
+    def cobroLiquidado(self, cobro, trans):
+        pass
+
+    def inguzInguz(self, inguz, trans):
+        pass
+
+    def stpTransaccion(self, stp, trans):
+        pass
+
+    def speiTransaccion(self, trans):
+        pass
+
+    def scotiaDeposito(self, scotia, trans):
+        pass
+
+    def scotiaRetiro(self, scotia, trans):
+        pass
+
+    def ticketCancelacion(self, trans):
+        pass
