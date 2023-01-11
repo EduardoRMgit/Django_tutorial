886b317a2d22d80abb51c26f1637c4df65a36df3
Crow <ajimenez@invercratos.io>
2023-01-11 16:19:21 -0600

    Se genera el perfil transaccional

--- cactus/demograficos/admin/__init__.py
+++ cactus/demograficos/admin/__init__.py
@@ -5,6 +5,7 @@ from .contactosAdmin import *
 from .adminUtils import adminUtilsAdmin
 from .adminUtils import UPAdmin
 from .udeviceAdmin import UserLocation
+from .perfil import PerfilTransaccionalDeclaradoAdmin
 
 
 # TODO eliminar las importaciones asterico.
---
+++ cactus/demograficos/models/perfildeclarado.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class TransferenciasMensuales(models.Model):
+
+    transferencias_mensuales = models.CharField(null=True,
+                                                blank=True,
+                                                max_length=50)
+
+    def __str__(self):
+        return str(self.transferencias_mensuales)
+
+
+class OperacionesMensual(models.Model):
+
+    operaciones_mensuales = models.CharField(null=True,
+                                             blank=True,
+                                             max_length=50)
+
+    def __str__(self):
+        return str(self.operaciones_mensuales)
+
+
+class UsoCuenta(models.Model):
+
+    uso_de_cuenta = models.CharField(null=True,
+                                     blank=True,
+                                     max_length=50)
+
+    def __str__(self):
+        return str(self.uso_de_cuenta)
+
+
+class OrigenDeposito(models.Model):
+
+    origen = models.CharField(null=True,
+                              blank=True,
+                              max_length=50)
+
+    def __str__(self):
+        return str(self.origen)
+
+
+class PerfilTransaccionalDeclarado(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user_perfil'
+    )
+
+    transferencias_mensuales = models.ForeignKey(
+        TransferenciasMensuales,
+        on_delete=models.CASCADE,
+    )
+
+    operaciones_mensuales = models.ForeignKey(
+        OperacionesMensual,
+        on_delete=models.CASCADE,
+    )
+
+    uso_cuenta = models.ForeignKey(
+        UsoCuenta,
+        on_delete=models.CASCADE,
+    )
+
+    origen = models.ForeignKey(
+        OrigenDeposito,
+        on_delete=models.CASCADE,
+    )
+
+    status = (
+        ("Pendiente", "Pendiente"),
+        ("Aprobado", "Aprobado")
+    )
+
+    status_perfil = models.CharField(null=True,
+                                     blank=True,
+                                     max_length=50,
+                                     choices=status)
+
+    def __str__(self):
+        return str(self.user)
