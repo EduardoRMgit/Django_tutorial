d1e8cdd7e2c13ef23e8bf742f465f04c4a403d78
Crow <ajimenez@invercratos.io>
2023-02-01 16:32:56 -0600

    Ahi va el mailing, le falta todavia

--- cactus/demograficos/utils/correo.py
+++ cactus/demograficos/utils/correo.py
@@ -1,7 +1,5 @@
-from django.core.mail import EmailMultiAlternatives
 from cactus.settings import EMAIL_HOST_USER
 from django.core.mail import send_mail
-from django.template.loader import get_template
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
@@ -11,25 +9,18 @@ def mandar_email(correo, username):
     receptor = correo
     user = username
     print(correo, username)
-    d = {'username': user}
     subject = 'Bienvenido a Inguz'
-    html_content = render_to_string("mail/correo1.html",{'username': user})
-    print(html_content)
-    text_content = strip_tags(html_content)
+    html_message = render_to_string(f'mail/correo.html',
+                                   {'username': user})
+    body = strip_tags(html_message)
 
-    email = EmailMultiAlternatives(
+
+    send_mail(
         subject,
-        text_content,
+        body,
         EMAIL_HOST_USER,
-        [receptor]
+        [receptor],
+        auth_password='pipweonnrzertwfv',
+        fail_silently=False,
+        html_message=html_message
     )
-    email.attach_alternative(html_content, "text/html")
-    email.send()
-    # send_mail(
-    #     subject,
-    #     body,
-    #     EMAIL_HOST_USER,
-    #     [receptor],
-    #     auth_password='pipweonnrzertwfv',
-    #     fail_silently=False
-    # )
