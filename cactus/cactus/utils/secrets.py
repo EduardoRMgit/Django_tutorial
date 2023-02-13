34e8875d8438db7845b0060405c9ca2b80214eae
Crow <ajimenez@invercratos.io>
2023-02-13 13:32:53 -0600

    Adios hardcode

---
+++ cactus/cactus/utils/secrets.py
@@ -0,0 +1,18 @@
+import base64
+import environ
+
+
+def cluster_secret(key, value):
+    try:
+        from kubernetes import client, config
+        try:
+            config.load_kube_config()
+        except Exception:
+            config.load_incluster_config()
+        v1 = client.CoreV1Api()
+        secret = v1.read_namespaced_secret(key, 'default')
+        secret = base64.b64decode(secret.data[value]).decode('utf-8')
+    except Exception as ex:
+        env = environ.Env()
+        secret = env.str(value, 'a')
+    return secret
