79390b942fc383cafdc6e4a23c1f78c5ba6147a6
Crow <ajimenez@invercratos.io>
2023-03-14 23:16:13 -0600

    Tox

--- cactus/demograficos/views.py
+++ cactus/demograficos/views.py
@@ -8,6 +8,7 @@ from demograficos.models import (DocAdjunto,
                                  TipoComprobante)
 import boto3
 from django.conf import settings
+import os
 
 
 def upload_s3_docs(tipocomprobante, archivo, user):
@@ -26,11 +27,13 @@ def upload_s3_docs(tipocomprobante, archivo, user):
             aws_acces_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
             region_name="us-east-2")
-        client.upload_fileobj(archivo, "phototest420", file_path)
+        client.upload_fileobj(archivo,
+            settings.AWS_STORAGE_BUCKET_NAME,
+            file_path)
         file_url = client.generate_presigned_url(
             'get_object',
             Params={
-                'Bucket': "phototest420",
+                'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
                 'Key': file_path
             },
             ExpiresIn=1440,
@@ -49,11 +52,13 @@ def upload_s3_docs(tipocomprobante, archivo, user):
             aws_acces_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
             region_name="us-east-2")
-        client.upload_fileobj(archivo, "phototest420", file_path)
+        client.upload_fileobj(archivo,
+            settings.AWS_STORAGE_BUCKET_NAME,
+            file_path)
         file_url = client.generate_presigned_url(
             'get_object',
             Params={
-                'Bucket': "phototest420",
+                'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
                 'Key': file_path
             },
             ExpiresIn=1440,
@@ -72,11 +77,13 @@ def upload_s3_docs(tipocomprobante, archivo, user):
             aws_acces_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
             region_name="us-east-2")
-        client.upload_fileobj(archivo, "phototest420", file_path)
+        client.upload_fileobj(archivo,
+            settings.AWS_STORAGE_BUCKET_NAME,
+            file_path)
         file_url = client.generate_presigned_url(
             'get_object',
             Params={
-                'Bucket': "phototest420",
+                'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
                 'Key': file_path
             },
             ExpiresIn=1440,
@@ -95,11 +102,13 @@ def upload_s3_docs(tipocomprobante, archivo, user):
             aws_acces_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
             region_name="us-east-2")
-        client.upload_fileobj(archivo, "phototest420", file_path)
+        client.upload_fileobj(archivo,
+            settings.AWS_STORAGE_BUCKET_NAME,
+            file_path)
         file_url = client.generate_presigned_url(
             'get_object',
             Params={
-                'Bucket': "phototest420",
+                'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
                 'Key': file_path
             },
             ExpiresIn=1440,
@@ -123,11 +132,13 @@ def upload_s3ine(archivo, user, tipo):
             aws_acces_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
             region_name="us-east-2")
-        client.upload_fileobj(archivo, "phototest420", file_path)
+        client.upload_fileobj(archivo,
+            settings.AWS_STORAGE_BUCKET_NAME,
+            file_path)
         file_url = client.generate_presigned_url(
             'get_object',
             Params={
-                'Bucket': "phototest420",
+                'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
                 'Key': file_path
             },
             ExpiresIn=1440,
@@ -146,11 +157,13 @@ def upload_s3ine(archivo, user, tipo):
             aws_acces_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
             region_name="us-east-2")
-        client.upload_fileobj(archivo, "phototest420", file_path)
+        client.upload_fileobj(archivo,
+            settings.AWS_STORAGE_BUCKET_NAME,
+            file_path)
         file_url = client.generate_presigned_url(
             'get_object',
             Params={
-                'Bucket': "phototest420",
+                'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
                 'Key': file_path
             },
             ExpiresIn=1440,
@@ -160,7 +173,6 @@ def upload_s3ine(archivo, user, tipo):
 
 class ImageDoc(generics.CreateAPIView):
     serializer_class = ImageDocSerializer
-
 
     def post(self, request):
         imagen = request.data['imagen']
