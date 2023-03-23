fc4a2f81f8ceb6261cb5a583036a9ddc2077ae89
Irwin Alejandro Jaimes Campos <ijaimes@inguzmx.com>
2023-03-23 17:22:29 -0600

    otro avance, falta aun el template

--- cactus/banca/utils/comprobantesPng.py
+++ cactus/banca/utils/comprobantesPng.py
@@ -1,6 +1,5 @@
 import os
 import cv2
-from PIL import Image
 from banca.models import Comprobante, Transaccion
 from spei.models import StpTransaction
 from cactus.settings import MEDIA_ROOT
@@ -12,15 +11,15 @@ class CompTrans(object):
     def __init__(self, trans: Transaccion):
         self._trans = trans
         self._tipo = trans.tipoTrans
-        codigo = self._tipo.codigo
-        status = trans.statusTrans.nombre
-        if status == "exito":
-            codigo = codigo
-        if status == "rechazada":
-            codigo = int(codigo)
-            codigo += 1
-            codigo = str(codigo)
-        self._tp = Comprobante.objects.get(codigo=codigo).template
+        # codigo = self._tipo.codigo
+        self._status = trans.statusTrans.nombre
+        # if self._status == "exito":
+        #     codigo = codigo
+        # if self._status == "rechazada":
+        #     codigo = int(codigo)
+        #     codigo += 1
+        #     codigo = str(codigo)
+        self._tp = Comprobante.objects.get(codigo=20).template
         self._dir = os.path.abspath(os.path.join(MEDIA_ROOT, self._tp.name))
         self._tp = cv2.imread(self._dir)
 
@@ -39,7 +38,15 @@ class CompTrans(object):
         lista = [nombre, concepto, alias,
                  fecha, hora, monto, trans.claveRastreo]
 
-        img = cv2.imread(self._dir)
+        if settings.SITE == "local":
+            avatar = os.path.abspath(os.path.join(
+                MEDIA_ROOT, avatar.avatar_img.name))
+            avatar = cv2.imread(avatar, -1)
+            img = cv2.imread(self._dir)
+        elif settings.SITE not in "local":
+            avatar = avatar.avatar_img.name
+            avatar = imutils.url_to_image(avatar, -1)
+            img = imutils.url_to_image(self._dir)
         coord = [
             (220, 350),
             (119, 725),
@@ -70,14 +77,6 @@ class CompTrans(object):
                 grosorLetra
             )
 
-        # ComprobanteUsuario.objects.create(transaccion=trans, img=img1)
-        if settings.SITE == "local":
-            avatar = os.path.abspath(os.path.join(
-                MEDIA_ROOT, avatar.avatar_img.name))
-            avatar = cv2.imread(avatar, -1)
-        elif settings.SITE not in "local":
-            avatar = avatar.avatar_img.name
-            avatar = imutils.url_to_image(avatar, -1)
         avatar = cv2.resize(avatar, (250, 250))
         x_offset = 180
         y_offset = 0
@@ -89,7 +88,6 @@ class CompTrans(object):
             img[y1:y2, x1:x2, c] = (alpha_s * avatar[:, :, c] +
                                     alpha_l * img[y1:y2, x1:x2, c])
 
-        # if show:
         img_name = "comprobanteInguz.jpg"
         cv2.imwrite(img_name, img)
         comp_img = open(img_name, "rb")
@@ -98,58 +96,51 @@ class CompTrans(object):
 
     def cobro(self):
         trans = self._trans
+        nombre = trans.user.Uprofile.get_nombre_completo()
         alias = trans.user.Uprofile.alias
-        monto = round(float(trans.monto), 2)
+        status = self._status
+        avatar = trans.user.Uprofile.avatar
+        monto = str(round(float(trans.monto), 2))
         fecha = trans.fechaValor.strftime("%m/%d/%Y")
         hora = trans.fechaValor.strftime("%H:%M:%S")
         concepto = trans.concepto
-        print(alias, monto, fecha, hora, concepto)
 
-        # instance = self._trans
-
-        lista = [concepto, trans.claveRastreo, fecha, hora, monto, alias]
-
-        img = cv2.imread(self._dir)
-        coord = [
-            (118, 408),
-            (116, 513),
-            (118, 620),
-            (119, 725),
-            (385, 407),
-            (385, 514),
+        if settings.SITE == "local":
+            avatar = os.path.abspath(os.path.join(
+                MEDIA_ROOT, avatar.avatar_img.name))
+            avatar = cv2.imread(avatar, -1)
+            img = cv2.imread(self._dir)
+        elif settings.SITE not in "local":
+            avatar = avatar.avatar_img.name
+            avatar = imutils.url_to_image(avatar, -1)
+            img = imutils.url_to_image(self._dir)
+        fields = [
+            [nombre,    (210, 350), 2, 0.9, (0, 0, 0), 1, 16],
+            [concepto,  (118, 700), 2, 0.9, (0, 0, 0), 1, 16],
+            [status,    (360, 490), 2, 0.9, (0, 0, 0), 1, 16],
+            [trans.claveRastreo,  (118, 800), 2, 0.9, (0, 0, 0), 1, 16],
+            [fecha,     (118, 600), 2, 0.9, (0, 0, 0), 1, 16],
+            [hora,      (350, 600), 2, 0.9, (0, 0, 0), 1, 16],
+            [monto,     (118, 490), 2, 1.0, (0, 0, 0), 2, 16],
+            [alias,     (220, 380), 2, 0.9, (0, 0, 1), 0, 16],
         ]
-        for i in range(len(lista)):
-            texto = str(lista[i])
-            ubicacion = coord[i]
-            font = cv2.FONT_ITALIC
-            tamañoLetra = 1
-            colorLetra = (35, 82, 50)
-            grosorLetra = 2
 
-            cv2.putText(
-                img,
-                texto,
-                ubicacion,
-                font,
-                tamañoLetra,
-                colorLetra,
-                grosorLetra
-            )
-
-        # ComprobanteUsuario.objects.create(transaccion=trans, img=img1)
-        cv2.imwrite("hoja.jpg", img)
-        img1 = Image.open("hoja.jpg")
-        # img2 = Image.open("banca/OTROSBANCOS.png")
-        # img1.paste(img2, (650, 300), mask=img2)
-        sha = img1
-        # if show:
-        #     sha.show()
-        sha.save("otrorecibo.jpg", "JPEG")
-        comp_img = open("otrorecibo.jpg", "rb")
-        sha.show()
-        os.remove("hoja.jpg")
-        os.remove("otrorecibo.jpg")
-
+        for field in fields:
+            cv2.putText(img, *field)
+        avatar = cv2.resize(avatar, (250, 250))
+        x_offset = 180
+        y_offset = 0
+        y1, y2 = y_offset, y_offset + avatar.shape[0]
+        x1, x2 = x_offset, x_offset + avatar.shape[1]
+        alpha_s = avatar[:, :, 3] / 255.0
+        alpha_l = 1.0 - alpha_s
+        for c in range(0, 3):
+            img[y1:y2, x1:x2, c] = (alpha_s * avatar[:, :, c] +
+                                    alpha_l * img[y1:y2, x1:x2, c])
+        img_name = "comprobanteInguz.jpg"
+        cv2.imwrite(img_name, img)
+        comp_img = open(img_name, "rb")
+        os.remove(img_name)
         return comp_img
 
     def cobroL(self, cobro):
@@ -175,7 +166,7 @@ class CompTrans(object):
             [hora,     (378,  590), 2, 0.9, (0, 0, 0), 1, 16],
             [concepto, (112,  690), 2, 0.9, (0, 0, 0), 1, 16],
         ]
-        if trans.statusTrans.nombre == 'exito':
+        if self._trans == 'exito':
             field_ = [
                 [rastreo,  (112, 800), 2, 0.9, (0, 0, 0), 1, 16],
                 [referencia, (112, 900), 2, 0.9, (0, 0, 0), 1, 16],
@@ -183,10 +174,10 @@ class CompTrans(object):
             for field in field_:
                 fields.append(field)
 
-        # img = imutils.url_to_image("https://i.pinimg.com/736x/87{}".format(
-        #         "/2b/46/872b463134db6ed9bb62c59dc2191a2e.jpg"))
-        img = cv2.imread(self._dir)
-
+        if settings.SITE == "local":
+            img = cv2.imread(self._dir)
+        elif settings.SITE not in "local":
+            img = imutils.url_to_image(self._dir)
         for field in fields:
             cv2.putText(img, *field)
         otrosbancos = imutils.url_to_image(
@@ -226,11 +217,12 @@ class CompTrans(object):
         pass
 
     def trans(self):
-        if self._tipo.codigo == "18":
+        # if self._tipo.codigo == "18":
 
-            return self.inguz()
-        if self._tipo.codigo == "20":
-            return self.cobro()
+        #     return self.inguz()
+        # if self._tipo.codigo == "20":
+        #     return self.cobro()
 
-        if self._tipo.codigo == "2":
-            return self.stp()
+        # if self._tipo.codigo == "2":
+        #     return self.stp()
+        return self.cobro()
