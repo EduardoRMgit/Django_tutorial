25cf012ed7d1a1c8c35b1098ac750966979c0145
Irwin Alejandro Jaimes Campos <ijaimes@inguzmx.com>
2023-04-25 12:26:00 -0600

    se esconde dominio y se quita url por default

--- cactus/cactus/settings.py
+++ cactus/cactus/settings.py
@@ -352,7 +352,6 @@ PREFIJO_CUENTA_INGUZ = "6461802180"
 
 AXES_LOCKOUT_CALLABLE = "cactus.customAuthBackend.lockout"
 
-URL_IMAGEN = "https://phototest420.s3.amazonaws.com/docs/docs/banca/comprobantes/comprobante_ejemplo.jpeg"
 
 
 def cluster_secret(key, value):
