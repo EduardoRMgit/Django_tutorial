198a6cedf3ffd38075bf708d1f3f00e8fca948e4
Irwin Alejandro Jaimes Campos <ijaimes@inguzmx.com>
2023-05-03 18:34:19 -0600

    se valida por separado la del dia

--- cactus/banca/utils/cobroComision.py
+++ cactus/banca/utils/cobroComision.py
@@ -6,21 +6,31 @@ from banca.models import Transaccion
 def comisionSTP(instance):
     today = datetime.now()
     monto = instance.monto
-    transacciones = Transaccion.objects.filter(
+    transacciones_d = Transaccion.objects.filter(
             tipoTrans__tipo="E",
             tipoTrans__medio="T",
             fechaValor__year=today.year,
             fechaValor__month=today.month,
-            statusTrans__nombre__in=["exito"]
+            fechaValor__day=today.day,
+            statusTrans__nombre__in=["exito"],
+            user=instance.user
         )
-    valida_primera = transacciones.count()
+    transacciones_t = Transaccion.objects.filter(
+            tipoTrans__tipo="E",
+            tipoTrans__medio="T",
+            fechaValor__year=today.year,
+            fechaValor__month=today.month,
+            statusTrans__nombre__in=["exito"],
+        )
+    valida_primera = transacciones_d.count()
+    valida_total = transacciones_t.count()
     if valida_primera >= 1:
         comisiones = ComisioneSTP.objects.all()
         for comision in comisiones:
             if comision.rangotransacciones:
                 rango = (comision.rangotransacciones).split("-")
-                if valida_primera >= int(
-                        rango[0]) and valida_primera <= int(rango[1]):
+                if valida_total >= int(
+                        rango[0]) and valida_total <= int(rango[1]):
                     instance.comision = ComisioneSTP.objects.get(
                         id=comision.id)
                     instance.save()
