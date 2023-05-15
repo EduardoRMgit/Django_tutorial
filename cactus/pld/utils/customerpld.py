6e8e802bc8480b3f76a714ef03ff8ab00aa3e9b6
Crow <ajimenez@invercratos.io>
2023-05-15 10:54:40 -0600

    fix: sintaxis

--- cactus/pld/utils/customerpld.py
+++ cactus/pld/utils/customerpld.py
@@ -140,7 +140,7 @@ def create_pld_customer(user):
                 correo_electronico=user.email,
                 actua_cuenta_propia=1,
                 mensaje=content_customer['response_api']['message'],
-                no_cliente=no_cliente=user.Uprofile.cuentaClabe[10:17],
+                no_cliente=user.Uprofile.cuentaClabe[10:17],
                 riesgo=content_customer[
                     'response_api']['customer_repet']['riesgo'],
                 user=user,
