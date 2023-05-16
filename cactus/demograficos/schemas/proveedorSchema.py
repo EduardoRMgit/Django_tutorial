a0a0d8994518138843028328b51036bebfe00fc8
Irwin Alejandro Jaimes Campos <ijaimes@inguzmx.com>
2023-05-15 19:14:31 -0600

    se agrega validacion de curp y se autollena

--- cactus/demograficos/schemas/proveedorSchema.py
+++ cactus/demograficos/schemas/proveedorSchema.py
@@ -1,7 +1,13 @@
 import graphene
 from demograficos.models import Proveedor
-
+from renapo.renapo_call import check_renapo
 from graphene_django.types import DjangoObjectType
+from django.conf import settings
+from demograficos.utils.stringNormalize import normalize
+import logging
+
+
+db_logger = logging.getLogger("db")
 
 
 class ProveedorType(DjangoObjectType):
@@ -30,7 +36,27 @@ class ProveedorSchema(graphene.Mutation):
         if curp is not None:
             curp = curp.upper()
             # Agregar validación
-
+        if settings.SITE in ["prod"]:
+            data, mensaje = check_renapo(curp)
+        else:
+            data = {
+                'nombre_renapo': "None",
+                'ap_pat_renapo': "None",
+                'ap_mat_renapo': "None",
+                'fechNac_renapo': "None"
+            }
+            mensaje = ""
+        try:
+            if data:
+                nombre_renapo = normalize(data['nombre_renapo'])
+                ap_pat_renapo = normalize(data['ap_pat_renapo'])
+                ap_mat_renapo = normalize(data['ap_mat_renapo'])
+                fechNac_renapo = data['fechNac_renapo']
+        except Exception as ex:
+            db_logger = logging.getLogger('db')
+            mensaje = "[CONSULTA CURP RENAPO] Falló la validación. Error: \
+                {} / mensaje: {}.".format(ex, mensaje)
+            db_logger.error(mensaje)
         user = info.context.user
         if user.is_anonymous:
             raise AssertionError('usuario no identificado')
@@ -38,12 +64,12 @@ class ProveedorSchema(graphene.Mutation):
             user.email = correo if correo else user.email
         proveedor = Proveedor.objects.create(
             user=user,
-            nombre="from curp",
-            apellido_p="from cppurp",
-            apellido_m="from curp",
+            nombre=nombre_renapo,
+            apellido_p=ap_pat_renapo,
+            apellido_m=ap_mat_renapo,
             correo_electronico=correo,
             curp=curp,
-            entidad_nacimiento="from curp",
+            entidad_nacimiento=fechNac_renapo,
             ocupacion=ocupacion,
             parentesco=parentesco
         )
