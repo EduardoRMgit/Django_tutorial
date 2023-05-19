e9772fc75fcf4cbad0c894c154f01cbb2f2755fc
Crow <ajimenez@invercratos.io>
2023-05-19 15:09:01 -0600

    add: tentative cronjob

---
+++ cactus/scripts/conciliacionRecargas.py
@@ -0,0 +1,5 @@
+from arcus.utils.conciliacionrecargas import conciliacion_arcus_recargas
+
+
+def run():
+    conciliacion_arcus_recargas()
---
+++ cactus/scripts/conciliacionServicios.py
@@ -0,0 +1,5 @@
+from arcus.utils.conciliacionpagos import conciliacion_arcus_pagos
+
+
+def run():
+    conciliacion_arcus_pagos()
