aa09972a0bb0778739b94c22e9e23c9f1e918780
Crow <ajimenez@invercratos.io>
2023-05-19 16:24:37 -0600

    sin cron

--- cactus/scripts/conciliacionRecargas.py
+++
@@ -1,5 +0,0 @@
-from arcus.utils.conciliacionrecargas import conciliacion_arcus_recargas
-
-
-def run():
-    conciliacion_arcus_recargas()
--- cactus/scripts/conciliacionServicios.py
+++
@@ -1,5 +0,0 @@
-from arcus.utils.conciliacionpagos import conciliacion_arcus_pagos
-
-
-def run():
-    conciliacion_arcus_pagos()
