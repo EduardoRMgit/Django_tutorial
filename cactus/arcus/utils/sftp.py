5e2fa4fa3585dd351eb9f71bb75015faa1b7fc2e
Crow <ajimenez@invercratos.io>
2023-06-16 17:16:28 -0600

    fix: trendmicro library and arcus cron function

--- cactus/arcus/utils/sftp.py
+++ cactus/arcus/utils/sftp.py
@@ -3,7 +3,7 @@ from arcus.utils.uploadarcus import upload_arcus
 from cactus.settings import (ARCUS_HOST,
                              ARCUS_USER,
                              ARCUS_PASS)
-import pysftp
+import paramiko
 import os
 
 
@@ -12,10 +12,19 @@ def sftp_connect():
     doc, contenido = conciliacion_arcus_pagos()
 
     path = os.path.join('', doc.name)
-    with pysftp.Connection(ARCUS_HOST,
-                           username=ARCUS_USER,
-                           password=ARCUS_PASS) as sftp:
-        with sftp.cd('IN'):
-            sftp.put(path)
+
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ARCUS_HOST,
+                   port='22',
+                   username=ARCUS_USER,
+                   password=ARCUS_PASS)
+    sftp = client.open_sftp()
+    sftp.chdir('IN')
+    archivo_conciliacion = sftp.file(path, 'w')
+    archivo_conciliacion.write(contenido)
+    archivo_conciliacion.close()
+    sftp.close()
+    client.close()
     upload_arcus(path, contenido)
     os.remove(path)
