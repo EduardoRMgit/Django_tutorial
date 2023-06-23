4ed735075a261f57f40565b1f887ea3cd8d836a0
albertkanbalam <albertkanbalam@gmail.com>
2023-06-23 05:48:37 -0600

    add: pagoo de prestamos zaki por abonos

--- cactus/banca/utils/pago_prestamo_zaki.py
+++ cactus/banca/utils/pago_prestamo_zaki.py
@@ -20,6 +20,7 @@ def crear_prestamo(request):
     # {"username": "5573973545", "amount": 500.00}
     username = request['username']
     abono = request['amount']
+    monto_total = request['total_amount']
 
     try:
         ordenante = User.objects.get(username=settings.USERNAME_ZAKI)
@@ -27,15 +28,13 @@ def crear_prestamo(request):
         db_logger.warning(
             f"[ZAKI crear_prestamo] Usuario Zaki no encontrado. ex: {ex}")
         return {
-            'error': -24710,
-            # 'msg': "Usuario Zaki no encontrado",
+            'error': -24710
         }
 
     if float(abono) > ordenante.Uprofile.saldo_cuenta:
         db_logger.warning("[ZAKI crear_prestamo] Saldo Zaki insuficiente")
         return {
-            'error': -24711,
-            # 'msg': "Saldo Zaki insuficiente",
+            'error': -24711
         }
 
     user_beneficiario = User.objects.filter(
@@ -45,8 +44,7 @@ def crear_prestamo(request):
         db_logger.warning(
             f"[ZAKI crear_prestamo] User {username} no encontrado o inactivo.")
         return {
-            'error': -24712,
-            # 'msg': "Usuario no encontrado",
+            'error': -24712
         }
     user_beneficiario = user_beneficiario.last()
 
@@ -56,8 +54,7 @@ def crear_prestamo(request):
         db_logger.warning(
             f"[ZAKI crear_prestamo] User {username} ya cuenta con préstamo.")
         return {
-            'error': -24714,
-            # 'msg': "Usuario no encontrado",
+            'error': -24714
         }
 
     fecha = datetime.now()
@@ -67,6 +64,7 @@ def crear_prestamo(request):
         codigo=201)  # Prestamo recibida
     claveR = randomString()
     monto2F = "{:.2f}".format(round(float(abono), 2))
+    monto_total2F = "{:.2f}".format(round(float(monto_total), 2))
 
     enviada_trans = Transaccion.objects.create(
         user=ordenante,
@@ -88,22 +86,20 @@ def crear_prestamo(request):
         concepto="Préstamo Zaki",
         claveRastreo=claveR
     )
-    PrestamoZakiTransaccion.objects.create(
+    prestamo = PrestamoZakiTransaccion.objects.create(
         user=user_beneficiario,
         fechaOperacion=fecha,
         enviada_prestamo=enviada_trans,
         recibida_prestamo=recibida_trans,
         monto=monto2F,
+        monto_total=monto_total2F,
         status="P"
     )
 
-    # Hacerlos atómicos (en general desde antes de los creates)
-    # if_check_abono_>_saldo:
     ordenante.Uprofile.saldo_cuenta -= round(float(abono), 2)
     ordenante.Uprofile.save()
     user_beneficiario.Uprofile.saldo_cuenta += round(float(abono), 2)
     user_beneficiario.Uprofile.save()
-    # else_check: warning and raise
 
     msg = "[Zaki crear_prestamo] Exitoso para el usuario: {} por {}".format(
         username,
@@ -111,23 +107,31 @@ def crear_prestamo(request):
     db_logger.info(msg)
 
     return {
-        "created": True
+        "created": True,
+        "inguzId": prestamo.id
     }
 
 
-def liquidar_prestamo(request):
+def abonar_prestamo(request):
     # Permissions: Sólo permitida al usuario Zaki
-    # {"username": "5529641640"}
+    # {"username": "5529641640", "amount": 50}
+    if 'username' not in request or 'amount' not in request:
+        db_logger.warning(
+            f"[ZAKI abonar_prestamo] Bad request, from zaki: {request}")
+
+        return {
+            'error': -24719
+        }
     username = request['username']
+    abono = request['amount']
 
     try:
         user_zaki = User.objects.get(username=settings.USERNAME_ZAKI)
     except Exception as ex:
         db_logger.warning(
-            f"[ZAKI liquidar_prestamo] Usuario Zaki no encontrado. ex: {ex}")
+            f"[ZAKI abonar_prestamo] Usuario Zaki no encontrado. ex: {ex}")
         return {
-            'error': -24713,
-            # 'msg': "Usuario Zaki no encontrado",
+            'error': -24713
         }
 
     ordenante = User.objects.filter(
@@ -135,39 +139,46 @@ def liquidar_prestamo(request):
         is_active=True)
     if ordenante.count() == 0:
         db_logger.warning(
-            f"[ZAKI liquidar_prest] User {username} no encontrado o inactivo.")
+            f"[ZAKI abono_prestamo] User {username} no encontrado o inactivo.")
         return {
-            'error': -24715,
-            # 'msg': "Usuario no encontrado",
+            'error': -24715
         }
     ordenante = ordenante.last()
     prestamos = ordenante.prestamos_zaki.all()
     num_prestamos = prestamos.filter(status="P").count()
     if num_prestamos == 0:
         db_logger.warning(
-            f"[ZAKI liquidar_prestamo] User {username} sin préstamo activo.")
+            f"[ZAKI abonar_prestamo] User {username} sin préstamo activo.")
         return {
-            'error': -24716,
-            # 'msg': "Usuario sin préstamos",
+            'error': -24716
         }
 
     if num_prestamos > 1:
         db_logger.warning(
-            f"[ZAKI liquidar_presta] User {username} con múltiples préstamos.")
+            f"[ZAKI abonar_prestamo] User {username} con múltiples préstamos.")
         return {
-            'error': -24717,
-            # 'msg': "Usuario con múltiples préstamos",
+            'error': -24717
         }
 
     prestamo = prestamos.get(status="P")
-    abono = prestamo.monto
+
     if float(abono) > ordenante.Uprofile.saldo_cuenta:
         db_logger.warning(
-            "[ZAKI liquidar_prestamo] Saldo Usuario insuficiente")
+            "[ZAKI abonar_prestamo] Saldo Usuario insuficiente")
         return {
-            'error': -24718,
-            # 'msg': "Saldo Usuario insuficiente",
+            'error': -24718
         }
+
+    balance_previo = sum(
+        list(
+            map(
+                lambda _abono: float(_abono.monto), prestamo.abonos.all()
+            )
+        )
+    )
+
+    if (balance_previo + float(abono)) > float(prestamo.monto_total):
+        abono = float(prestamo.monto_total) - float(balance_previo)
 
     fecha = datetime.now()
     status = StatusTrans.objects.get(nombre="exito")
@@ -184,7 +195,7 @@ def liquidar_prestamo(request):
         monto=float(abono),
         statusTrans=status,
         tipoTrans=tipo_enviada,
-        concepto="Pago Zaki",
+        concepto="Abono Zaki",
         claveRastreo=claveR
     )
     recibida_trans = Transaccion.objects.create(
@@ -194,32 +205,47 @@ def liquidar_prestamo(request):
         monto=float(abono),
         statusTrans=status,
         tipoTrans=tipo_recibida,
-        concepto="Pago Zaki",
+        concepto="Abono Zaki",
         claveRastreo=claveR
     )
-    PagoZakiTransaccion.objects.create(
+    pago = PagoZakiTransaccion.objects.create(
+        prestamo=prestamo,
         user=ordenante,
         fechaOperacion=fecha,
         enviada_pago=enviada_trans,
         recibida_pago=recibida_trans,
         monto=monto2F
     )
-
-    # Hacerlos atómicos (en general desde antes de los creates)
-    # if_check_abono_>_saldo:
     ordenante.Uprofile.saldo_cuenta -= round(float(abono), 2)
     ordenante.Uprofile.save()
     user_zaki.Uprofile.saldo_cuenta += round(float(abono), 2)
     user_zaki.Uprofile.save()
-    prestamo.status = "L"
-    prestamo.save()
-    # else_check: warning and raise
 
-    msg = "[Zaki liquidar_prestamo] Exitoso para el usuario: {} por {}".format(
+    msg = "[Zaki abonar_prestamo] abono para el usuario: {} por {}".format(
         username,
         abono)
     db_logger.info(msg)
 
+    balance = sum(
+        list(
+            map(
+                lambda abono: float(abono.monto), prestamo.abonos.all()
+            )
+        )
+    )
+
+    liquidado = False
+    if balance == float(prestamo.monto_total):
+        prestamo.status = "L"
+        prestamo.save()
+        liquidado = True
+        msg = "[Zaki abonar_prestamo] Liquidado para user: {} por {}".format(
+            username,
+            abono)
+        db_logger.info(msg)
+
     return {
-        "created": True
+        "created": True,
+        "inguzId": pago.id,
+        "liquidado": liquidado
     }
