292a32d05e8454db54e7ab74b034453867291de9
Irwin Alejandro Jaimes Campos <ijaimes@inguzmx.com>
2023-06-29 13:49:20 -0600

    fix: se agrega comision

--- cactus/banca/schemas/cepSchema.py
+++ cactus/banca/schemas/cepSchema.py
@@ -7,7 +7,14 @@ from spei.cep import url_cep
 class UrlCepType(DjangoObjectType):
     class Meta:
         model = StpTransaction
+    comision = graphene.Float()
 
+    def resolve_comision(self, info):
+        if self.transaccion.comision:
+            comision = 7.52
+        else:
+            comision = 0
+        return comision
 
 class Query(graphene.ObjectType):
     cep = graphene.Field(UrlCepType,
