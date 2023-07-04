8e3df61604d808b05c9c283e8a546c4e8b898be9
EduardoRMgit <preyas89_lalo@hotmail.com>
2023-07-03 18:49:54 -0600

    TokenD_ScotiaSchema

--- cactus/banca/schemas/ScotiaSchema.py
+++ cactus/banca/schemas/ScotiaSchema.py
@@ -27,6 +27,7 @@ from scotiabank.models import (ScotiaTransferencia,
                                DatosFijosPDF)
 
 from demograficos.models.userProfile import UserProfile
+from demograficos.utils.tokendinamico import validaToken
 
 
 def upload_s3(nombre_archivo, archivo):
@@ -174,7 +175,7 @@ class CreateTransferenciaScotia(graphene.Mutation):
         rfcCurpBeneficiario = graphene.String()
         clave_beneficiario = graphene.String(required=True)
         concepto = graphene.String(required=True)
-        nip = graphene.String()
+        token_d = graphene.String()
         ubicacion = graphene.String()
 
     @login_required
@@ -187,7 +188,7 @@ class CreateTransferenciaScotia(graphene.Mutation):
                clave_beneficiario,
                concepto,
                ubicacion,
-               nip,
+               token_d,
                tipoCuentaBeneficiario=None,
                rfcCurpBeneficiario=None):
         try:
@@ -196,48 +197,53 @@ class CreateTransferenciaScotia(graphene.Mutation):
             raise Exception('Usuario inexistente')
         if UserProfile.objects.filter(user=ordenante).count() == 0:
             raise Exception('Usuario sin perfil')
-        if ordenante.Uprofile.password:
-            if not ordenante.Uprofile.check_password(nip):
-                raise Exception('Nip esta mal')
-        if monto == 0 or monto is None:
-            raise Exception('Ingrese un monto válido')
-        try:
-            institucionBeneficiariaInt = (
-                InstitutionBanjico.objects.get(id=institucionBeneficiariaInt))
-        except Exception:
-            raise Exception("Institucion no valida")
-        comision = 4.00
-        fecha = utc_to_local(timezone.now()).strftime('%Y-%m-%d %H:%M:%S')
-        claveR = randomString()
-        status = StatusTrans.objects.get(nombre="esperando respuesta")
-        tipo = TipoTransaccion.objects.get(codigo=16)
-        main_trans = Transaccion.objects.create(
-            user=ordenante,
-            fechaValor=fecha,
-            monto=float(comision),  # Revisar si incluye comisión
-            statusTrans=status,
-            tipoTrans=tipo,
-            concepto=concepto,
-            claveRastreo=claveR
-        )
+        # if ordenante.Uprofile.password:
+        #     if not ordenante.Uprofile.check_password(nip):
+        #         raise Exception('Nip esta mal')
+        if not ordenante.is_anonymous:
+            token = validaToken(ordenante, token_d)
+            if token:
+                if monto == 0 or monto is None:
+                    raise Exception('Ingrese un monto válido')
+                try:
+                    institucionBeneficiariaInt = (
+                        InstitutionBanjico.objects.get(
+                            id=institucionBeneficiariaInt))
+                except Exception:
+                    raise Exception("Institucion no valida")
+                comision = 4.00
+                fecha = utc_to_local(
+                    timezone.now()).strftime('%Y-%m-%d %H:%M:%S')
+                claveR = randomString()
+                status = StatusTrans.objects.get(nombre="esperando respuesta")
+                tipo = TipoTransaccion.objects.get(codigo=16)
+                main_trans = Transaccion.objects.create(
+                    user=ordenante,
+                    fechaValor=fecha,
+                    monto=float(comision),  # Revisar si incluye comisión
+                    statusTrans=status,
+                    tipoTrans=tipo,
+                    concepto=concepto,
+                    claveRastreo=claveR
+                )
 
-        scotia_transferencia = ScotiaTransferencia.objects.create(
-            transaccion=main_trans,
-            institucionBeneficiariaInt=institucionBeneficiariaInt,
-            monto=monto,
-            ordenante=ordenante,
-            nombreBeneficiario=nombreBeneficiario,
-            tipoCuentaBeneficiario=tipoCuentaBeneficiario,
-            cuentaBeneficiario=cuentaBeneficiario,
-            rfcCurpBeneficiario=rfcCurpBeneficiario,
-            clave_beneficiario=clave_beneficiario,
-            conceptoPago=concepto
-        )
+                scotia_transferencia = ScotiaTransferencia.objects.create(
+                    transaccion=main_trans,
+                    institucionBeneficiariaInt=institucionBeneficiariaInt,
+                    monto=monto,
+                    ordenante=ordenante,
+                    nombreBeneficiario=nombreBeneficiario,
+                    tipoCuentaBeneficiario=tipoCuentaBeneficiario,
+                    cuentaBeneficiario=cuentaBeneficiario,
+                    rfcCurpBeneficiario=rfcCurpBeneficiario,
+                    clave_beneficiario=clave_beneficiario,
+                    conceptoPago=concepto
+                )
 
-        return CreateTransferenciaScotia(
-            scotia_transferencia=scotia_transferencia,
-            user=ordenante
-        )
+                return CreateTransferenciaScotia(
+                    scotia_transferencia=scotia_transferencia,
+                    user=ordenante
+                )
 
 
 class CreateRetiroScotia(graphene.Mutation):
@@ -283,7 +289,7 @@ class CreateRetiroScotia(graphene.Mutation):
     class Arguments:
         token = graphene.String(required=True)
         monto = graphene.Float(required=True)
-        nip = graphene.String()
+        token_d = graphene.String()
         ubicacion = graphene.String()
 
     @login_required
@@ -291,7 +297,7 @@ class CreateRetiroScotia(graphene.Mutation):
                token,
                monto,
                ubicacion,
-               nip):
+               token_d):
 
         def _valida(expr, msg):
             if expr:
@@ -310,80 +316,85 @@ class CreateRetiroScotia(graphene.Mutation):
                 'Usuario sin perfil')
         _valida(user.Uprofile.password is None,
                 'El usuario no ha establecido su NIP.')
-        _valida(not user.Uprofile.check_password(nip),
-                'El NIP es incorrecto.')
-        _valida(monto <= 0 or monto is None,
-                'Únicamente montos positivos o válidos.')
-        _valida(saldo_inicial_usuario - (monto + comision) < 0,
-                'Saldo insuficiente.')
-        _valida(
-            not LimiteTrans(user.id).ret_efectivo_dia(float(monto)),
-            'Límite transaccional superado'
-        )
+        # _valida(not user.Uprofile.check_password(nip),
+        #         'El NIP es incorrecto.')
+        if not user.is_anonymous:
+            token = validaToken(user, token_d)
+            if token:
+                _valida(monto <= 0 or monto is None,
+                        'Únicamente montos positivos o válidos.')
+                _valida(saldo_inicial_usuario - (monto + comision) < 0,
+                        'Saldo insuficiente.')
+                _valida(
+                    not LimiteTrans(user.id).ret_efectivo_dia(float(monto)),
+                    'Límite transaccional superado'
+                )
 
-        clave_ordenante = ""
-        clabe = UserProfile.objects.get(user=user).cuentaClabe
-        for digito in clabe:
-            if digito != "0":
-                clave_ordenante += digito
+                clave_ordenante = ""
+                clabe = UserProfile.objects.get(user=user).cuentaClabe
+                for digito in clabe:
+                    if digito != "0":
+                        clave_ordenante += digito
 
-        # hoy = datetime.now()
-        # tiempo = time(15, 00, 00)
-        # _valida(
-        #     not (
-        #         hoy.weekday() in range(
-        #             0, 5) and datetime.now().time() <= tiempo),
-        #     'Fuera del horario de operacion')
-        # _valida(hoy.weekday() in range(5, 7), 'No se opera fines de semana')
+                # hoy = datetime.now()
+                # tiempo = time(15, 00, 00)
+                # _valida(
+                #     not (
+                #         hoy.weekday() in range(
+                #             0, 5) and datetime.now().time() <= tiempo),
+                #     'Fuera del horario de operacion')
+                # _valida(hoy.weekday() in range(5, 7),
+                #         'No se opera fines de semana')
 
-        fecha = utc_to_local(timezone.now()).strftime('%Y-%m-%d %H:%M:%S')
-        claveR = randomString()
-        status = StatusTrans.objects.get(nombre="esperando respuesta")
-        tipo = TipoTransaccion.objects.get(codigo=6)
-        reservado_scotia_trans = round(monto, 2)
-        fecha_t = utc_to_local(timezone.now()).strftime("%Y%m%d")
+                fecha = utc_to_local(
+                    timezone.now()).strftime('%Y-%m-%d %H:%M:%S')
+                claveR = randomString()
+                status = StatusTrans.objects.get(nombre="esperando respuesta")
+                tipo = TipoTransaccion.objects.get(codigo=6)
+                reservado_scotia_trans = round(monto, 2)
+                fecha_t = utc_to_local(timezone.now()).strftime("%Y%m%d")
 
-        # Se crea transacción padre sólo por la comisión
-        main_trans = Transaccion.objects.create(
-            user=user,
-            fechaValor=fecha,
-            monto=float(float(monto) + comision),
-            statusTrans=status,
-            tipoTrans=tipo,
-            concepto="Retiro de efectivo",
-            claveRastreo=claveR
-        )
+                # Se crea transacción padre sólo por la comisión
+                main_trans = Transaccion.objects.create(
+                    user=user,
+                    fechaValor=fecha,
+                    monto=float(float(monto) + comision),
+                    statusTrans=status,
+                    tipoTrans=tipo,
+                    concepto="Retiro de efectivo",
+                    claveRastreo=claveR
+                )
 
-        # Se crea saldo reservado sólo del monto sin comisión
-        scotia_retiro_reservado = SaldoReservado.objects.create(
-            tipoTrans=tipo,
-            status_saldo="reservado",
-            saldo_reservado=reservado_scotia_trans
-        )
+                # Se crea saldo reservado sólo del monto sin comisión
+                scotia_retiro_reservado = SaldoReservado.objects.create(
+                    tipoTrans=tipo,
+                    status_saldo="reservado",
+                    saldo_reservado=reservado_scotia_trans
+                )
 
-        scotia_retiro = ScotiaRetiro.objects.create(
-            ordenante=user,
-            transaccion=main_trans,
-            monto=Decimal(monto),
-            conceptoPago="Retiro Cliente",
-            ubicacion=ubicacion,
-            comision=Decimal(comision),
-            clave_retiro=str(clave_ordenante),
-            saldoReservado=scotia_retiro_reservado,
-            referenciaPago=str(fecha_t) + "{}".format(
-                '{:0>8}'.format(main_trans.id))
-        )
+                scotia_retiro = ScotiaRetiro.objects.create(
+                    ordenante=user,
+                    transaccion=main_trans,
+                    monto=Decimal(monto),
+                    conceptoPago="Retiro Cliente",
+                    ubicacion=ubicacion,
+                    comision=Decimal(comision),
+                    clave_retiro=str(clave_ordenante),
+                    saldoReservado=scotia_retiro_reservado,
+                    referenciaPago=str(fecha_t) + "{}".format(
+                        '{:0>8}'.format(main_trans.id))
+                )
 
-        # Quitamos el saldo que ya reservamos y el cobro de la comisión
-        user.Uprofile.saldo_cuenta = round(
-            float(saldo_inicial_usuario) - float(
-                float(reservado_scotia_trans) + float(comision)), 2)
-        user.Uprofile.save()
+                # Quitamos el saldo que ya reservamos y el cobro de la comisión
+                user.Uprofile.saldo_cuenta = round(
+                    float(saldo_inicial_usuario) - float(
+                        float(reservado_scotia_trans) + float(comision)), 2)
+                user.Uprofile.save()
 
-        return CreateRetiroScotia(
-            scotia_retiro=scotia_retiro,
-            user=user
-        )
+                return CreateRetiroScotia(
+                    scotia_retiro=scotia_retiro,
+                    user=user
+                )
 
 
 class CreateScotiaDeposito(graphene.Mutation):
@@ -438,7 +449,7 @@ class CreateScotiaDeposito(graphene.Mutation):
     class Arguments:
         token = graphene.String(required=True)
         monto = graphene.Float(required=True)
-        nip = graphene.String()
+        token_d = graphene.String()
         ubicacion = graphene.String()
 
     @login_required
@@ -446,7 +457,7 @@ class CreateScotiaDeposito(graphene.Mutation):
                info,
                token,
                monto,
-               nip,
+               token_d,
                ubicacion=None):
 
         def validar(expr, msg):
@@ -459,57 +470,63 @@ class CreateScotiaDeposito(graphene.Mutation):
             user=ordenante).count() == 0, 'Usuario sin perfil')
         validar(ordenante.Uprofile.password is None,
                 'El usuario no ha establecido su NIP.')
-        validar(not ordenante.Uprofile.check_password(nip),
-                'El NIP es incorrecto.')
-        validar(monto == 0 or monto is None, 'Ingrese un monto válido')
-        validar(
-            not LimiteTrans(ordenante.id).dep_efectivo_mes(float(monto)),
-            'Límite transaccional superado'
-        )
-        validar(
-            not LimiteTrans(ordenante.id).dep_efectivo_dia(float(monto)),
-            'Límite transaccional superado'
-        )
+        # validar(not ordenante.Uprofile.check_password(nip),
+        #         'El NIP es incorrecto.')
+        if not ordenante.is_anonymous:
+            token = validaToken(ordenante, token_d)
+            if token:
+                validar(monto == 0 or monto is None, 'Ingrese un monto válido')
+                validar(
+                    not LimiteTrans(
+                        ordenante.id).dep_efectivo_mes(float(monto)),
+                    'Límite transaccional superado'
+                )
+                validar(
+                    not LimiteTrans(
+                        ordenante.id).dep_efectivo_dia(float(monto)),
+                    'Límite transaccional superado'
+                )
 
-        hoy = timezone.now().date()
+                hoy = timezone.now().date()
 
-        num_depo = ScotiaDeposito.objects.filter(
-            ordenante=ordenante,
-            fecha_limite=hoy,
-            importe_documento=(monto)).count()
-        referencia_cobranza = genera_linea_de_captura(
-            ordenante.Uprofile.cuentaClabe,
-            hoy,
-            float(float(monto)),
-            num_depo + 1
-        )
-        fecha = utc_to_local(timezone.now()).strftime('%Y-%m-%d %H:%M:%S')
-        claveR = randomString()
-        status = StatusTrans.objects.get(nombre="esperando respuesta")
-        tipo = TipoTransaccion.objects.get(codigo=3)
-        main_trans = Transaccion.objects.create(
-            user=ordenante,
-            fechaValor=fecha,
-            monto=float(monto),
-            statusTrans=status,
-            tipoTrans=tipo,
-            concepto="Depósito de efectivo",
-            claveRastreo=claveR
-        )
-        scotia_deposito = ScotiaDeposito.objects.create(
-            ordenante=ordenante,
-            importe_documento=Decimal(float(float(monto))),
-            fecha_limite=hoy,
-            comision=Decimal(0),
-            referencia_cobranza=referencia_cobranza,
-            fecha_inicial=hoy,
-            ubicacion=ubicacion,
-            transaccion=main_trans
-        )
-        return CreateScotiaDeposito(
-            scotia_deposito=scotia_deposito,
-            user=ordenante
-        )
+                num_depo = ScotiaDeposito.objects.filter(
+                    ordenante=ordenante,
+                    fecha_limite=hoy,
+                    importe_documento=(monto)).count()
+                referencia_cobranza = genera_linea_de_captura(
+                    ordenante.Uprofile.cuentaClabe,
+                    hoy,
+                    float(float(monto)),
+                    num_depo + 1
+                )
+                fecha = utc_to_local(
+                    timezone.now()).strftime('%Y-%m-%d %H:%M:%S')
+                claveR = randomString()
+                status = StatusTrans.objects.get(nombre="esperando respuesta")
+                tipo = TipoTransaccion.objects.get(codigo=3)
+                main_trans = Transaccion.objects.create(
+                    user=ordenante,
+                    fechaValor=fecha,
+                    monto=float(monto),
+                    statusTrans=status,
+                    tipoTrans=tipo,
+                    concepto="Depósito de efectivo",
+                    claveRastreo=claveR
+                )
+                scotia_deposito = ScotiaDeposito.objects.create(
+                    ordenante=ordenante,
+                    importe_documento=Decimal(float(float(monto))),
+                    fecha_limite=hoy,
+                    comision=Decimal(0),
+                    referencia_cobranza=referencia_cobranza,
+                    fecha_inicial=hoy,
+                    ubicacion=ubicacion,
+                    transaccion=main_trans
+                )
+                return CreateScotiaDeposito(
+                    scotia_deposito=scotia_deposito,
+                    user=ordenante
+                )
 
 
 class Mutation(graphene.ObjectType):
